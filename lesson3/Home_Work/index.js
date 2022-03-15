f325333e0c73da61821676796338e481553e5960
Evgenii N <1_12@list.ru>
2022-03-15 20:03:58 +0300

    ДЗ 2 занятие

---
+++ lesson3/Home_Work/index.js
@@ -0,0 +1,75 @@
+// let value = true;
+// console.log(typeof value);
+
+
+// console.log(typeof String(value));
+
+// let userName = "Shanya";
+// console.log(`Hello ${userName}`);
+
+// let car={
+// model:"Hyundai",
+// enginePower:150,
+// color:"white",
+// }
+// const carProperty=car.enginePower;
+// console.log(carProperty);
+
+// console.log(typeof("Ivan"/4));
+
+// const obj = {
+// 	foo: "bar",
+// };
+// console.log(typeof(obj));
+
+// const string = "123zxc";
+// const number = +(string);
+// console.log(typeof number);
+
+// console.log(false==NaN);
+
+//п1
+let word = "Hello world";
+console.log(word);
+
+//п2
+let isAdmin = true;
+let string = "Hello";
+const number = 1337;
+let big = 21205n;
+let und = undefined;
+let zero = null;
+let ad = Symbol;
+
+var human ={
+age: 28,
+height : 193,
+sex : "male",
+}
+
+// п3
+console.log(isAdmin);
+console.log(string);
+console.log(number);
+console.log(big);
+console.log(und);
+console.log(zero);
+console.log(ad);
+console.log(human.age);
+
+// п4
+console.log(typeof isAdmin);
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof big);
+console.log(typeof und);
+console.log(typeof zero);
+console.log(typeof ad);
+console.log(typeof human.age);
+
+// п5
+// number=10;
+
+// п6
+human.age=21;
+console.log(human.age);
